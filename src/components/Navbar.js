4156645ca9e153d44df8539c3e9808f98fa0ed30
Vania Elizondo <vaniaelizondomartinez@gmail.com>
2024-11-07 11:15:27 -0600

    navbar mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,50 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import LanguageSelect from "./LanguageSelect";
+import { MdMenu } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
 
 export default function Navbar() {
   const { t, i18n } = useTranslation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  const closeMenu = () => setMenuOpen(false);
 
   const changeLanguage = (selectedLanguage) => {
     i18n.changeLanguage(selectedLanguage);
+    closeMenu();
   };
 
   return (
     <header>
-      <div className="title">
-        <Link to="/" className="name">
-          Héloïse Baland
-        </Link>
-        <div className="subtitle">{t("navbar.subtitle")}</div>
+      <div className="first-line">
+        <div className="title">
+          <Link to="/" className="name">
+            Héloïse Baland
+          </Link>
+          <div className="subtitle">{t("navbar.subtitle")}</div>
+        </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          {!menuOpen && <MdMenu />}
+          {menuOpen && <IoMdClose />}
+        </div>
       </div>
+      {menuOpen &&
+        <div className="navbar-mobile">
+          <ul>
+            <CustomLink to="/" closeMenu={closeMenu}>{t("navbar.home")}</CustomLink>
+            <CustomLink to="/gallery" closeMenu={closeMenu}>{t("navbar.gallery")}</CustomLink>
+            <CustomLink to="/cv" closeMenu={closeMenu}>{t("navbar.cv")}</CustomLink>
+            <CustomLink to="/contact" closeMenu={closeMenu}>{t("navbar.contact")}</CustomLink>
+            <li>
+              <LanguageSelect changeLanguage={changeLanguage} defaultLanguage={i18n.language} />
+            </li>
+          </ul>
+        </div>
+      }
       <div className="navbar">
         <nav>
           <ul>
@@ -32,13 +60,13 @@ export default function Navbar() {
   );
 }
 
-function CustomLink({ to, children, ...props }) {
+function CustomLink({ to, children, closeMenu, ...props }) {
   const resolvedPath = useResolvedPath(to);
   const isActive = useMatch({ path: resolvedPath.pathname, end: to === "/" });
 
   return (
     <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
+      <Link to={to} onClick={closeMenu} {...props}>
         {children}
       </Link>
     </li>
